338237a2b5d85e0fee34ba832c8b844f3b656d2c
unknown <suddhutrokka@gmail.com>
2024-02-04 16:20:43 +0545

    Python1100 - Code

---
+++ Day3 Python/add.py
@@ -0,0 +1,7 @@
+def add(a,b):
+    return a + b 
+x = 9
+y = 5
+total = add(a =x, b = y)
+print(total)
+
---
+++ Day3 Python/sub.py
@@ -0,0 +1,6 @@
+def sub(a,b):
+    return a - b 
+x = 9 
+y = 5
+total = sub(a =x, b = y)
+print(total)
